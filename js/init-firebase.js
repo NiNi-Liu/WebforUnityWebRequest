460178a9ae59c9d88af77e897a0560f0e2ca3f7f
NiNi-Liu <74700212+NiNi-Liu@users.noreply.github.com>
2021-06-05 13:39:55 +0800

    Add files via upload

---
+++ js/init-firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBW_P1dTW-yOAjc8-8sHxSVzo5O652rU9g",
+    authDomain: "myfirstdatabase-98cea.firebaseapp.com",
+    databaseURL: "https://myfirstdatabase-98cea.firebaseio.com",
+    projectId: "myfirstdatabase-98cea",
+    storageBucket: "myfirstdatabase-98cea.appspot.com",
+    messagingSenderId: "36630485541",
+    appId: "1:36630485541:web:c035614fbe9c9e549c7dcc",
+    measurementId: "G-G3CLDG2B4H"
+};
+firebase.initializeApp(firebaseConfig);
